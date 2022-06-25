9c6ecc76d0a36f94560fcbf83840e7b5828b9de3
Douglas Moura <douglas.ademoura@gmail.com>
2022-06-24 23:19:05 -0300

    feat: activate MSW on Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,9 @@
+import { createElement as h } from 'react'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { worker } from '../src/mocks/browser'
+
+worker.start()
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
@@ -7,3 +13,12 @@ export const parameters = {
     },
   },
 }
+
+
+const queryClient = new QueryClient()
+
+export const decorators = [
+  (Story) => (
+    h(QueryClientProvider, { client: queryClient }, h(Story))
+  )
+]
