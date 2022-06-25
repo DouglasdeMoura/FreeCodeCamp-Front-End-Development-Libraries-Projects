bccb3d2e238fd0a2dcbe552d2faa2e4e56bfd9a6
Douglas Moura <douglas.ademoura@gmail.com>
2022-06-24 21:17:40 -0300

    feat(api): add axios base configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import EnvironmentPlugin from 'vite-plugin-environment'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), EnvironmentPlugin(['API_BASE_URL'])],
 })
