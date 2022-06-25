1c8b33fb655b873c814d8f197a94d6a0397f6c31
Douglas Moura <douglas.ademoura@gmail.com>
2022-06-24 21:19:46 -0300

    fix(MSW on Jest): set the correct path

--- .jest/setup.js
+++ .jest/setup.js
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { setGlobalConfig } from '@storybook/testing-react'
 import { toHaveNoViolations } from 'jest-axe'
 import * as globalStorybookConfig from '../.storybook/preview'
-import { server } from '../src/mocks/server.js'
+import { server } from '../src/mocks/server'
 
 setGlobalConfig(globalStorybookConfig)
 expect.extend(toHaveNoViolations)
