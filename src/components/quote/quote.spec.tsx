b4705adeb5bc6c0572cce3fdf1cc609319df4e4b
Douglas Moura <douglas.ademoura@gmail.com>
2022-06-24 21:46:33 -0300

    chore: get quotes from api and enable Twitter sharing

--- src/components/quote/quote.spec.tsx
+++ src/components/quote/quote.spec.tsx
@@ -1,27 +1,38 @@
-import { querySelector, render, screen } from '../../utils/test-utils'
+import userEvent from '@testing-library/user-event'
+
+import {
+  querySelector,
+  customRender,
+  screen,
+  waitFor,
+} from '../../utils/test-utils'
 import { Quote } from './quote'
 
 const props = { text: 'mock_quote', author: 'mock_author' }
 
 describe('Random Quote Machine', () => {
+  afterEach(() => {
+    process.env.NEW_QUOTE = ''
+  })
+
   test('I can see a wrapper element with a corresponding id="quote-box"', () => {
-    const { container } = render(<Quote {...props} />)
+    const { container } = customRender(<Quote />)
 
     expect(querySelector(container, '#quote-box')).not.toBeNull()
   })
 
   test('Within #quote-box, I can see an element with corresponding id="text"', () => {
-    const { container } = render(<Quote {...props} />)
+    const { container } = customRender(<Quote />)
     expect(querySelector(container, '#quote-box #text')).not.toBeNull()
   })
 
   test('Within #quote-box, I can see an element with corresponding id="author".', () => {
-    const { container } = render(<Quote {...props} />)
+    const { container } = customRender(<Quote />)
     expect(querySelector(container, '#quote-box #author')).not.toBeNull()
   })
 
   test('Within #quote-box, I can see a clickable element with corresponding id="new-quote"', () => {
-    const { container } = render(<Quote {...props} />)
+    const { container } = customRender(<Quote />)
 
     expect(querySelector(container, '#quote-box #new-quote')).not.toBeNull()
 
@@ -32,7 +43,7 @@ describe('Random Quote Machine', () => {
   })
 
   test('Within #quote-box, I can see a clickable a element with a corresponding id="tweet-quote"', () => {
-    const { container } = render(<Quote {...props} />)
+    const { container } = customRender(<Quote />)
 
     expect(querySelector(container, '#quote-box #author')).not.toBeNull()
 
@@ -42,7 +53,7 @@ describe('Random Quote Machine', () => {
   })
 
   test('On first load, my quote machine displays a random quote in the element with id="text"', () => {
-    render(<Quote {...props} />)
+    customRender(<Quote />)
 
     const quoteBox = screen.getByText(props.text)
 
@@ -51,7 +62,7 @@ describe('Random Quote Machine', () => {
   })
 
   test('On first load, my quote machine displays the random quote\'s author in the element with id="author"', () => {
-    render(<Quote {...props} />)
+    customRender(<Quote />)
 
     const author = screen.getByText(props.author)
 
@@ -59,7 +70,47 @@ describe('Random Quote Machine', () => {
     expect(author).toHaveAttribute('id', 'author')
   })
 
-  test('When the #new-quote button is clicked, my quote machine should fetch a new quote and display it in the #text element', () => {
-    render(<Quote {...props} />)
+  test('When the #new-quote button is clicked, my quote machine should fetch a new quote and display it in the #text element', async () => {
+    customRender(<Quote />)
+    process.env.NEW_QUOTE = 'yes'
+    userEvent.click(screen.getByText(/New Quote/))
+
+    await waitFor(() => {
+      expect(screen.getByText('new_mock_quote')).toBeInTheDocument()
+    })
+
+    await waitFor(() => {
+      expect(screen.getByText('new_mock_quote')).toHaveAttribute('id', 'text')
+    })
+  })
+
+  test("My quote machine should fetch the new quote's author when the #new-quote button is clicked and display it in the #author element.", async () => {
+    customRender(<Quote />)
+    process.env.NEW_QUOTE = 'yes'
+    userEvent.click(screen.getByText(/New Quote/))
+
+    await waitFor(() => {
+      expect(screen.getByText('new_mock_author')).toBeInTheDocument()
+    })
+
+    await waitFor(() => {
+      expect(screen.getByText('new_mock_author')).toHaveAttribute(
+        'id',
+        'author',
+      )
+    })
+  })
+
+  test('I can tweet the current quote by clicking on the #tweet-quote a element. This a element should include the "twitter.com/intent/tweet" path in its href attribute to tweet the current quote', () => {
+    customRender(<Quote />)
+
+    const tweetThis = screen.getByText('Tweet this quote!')
+
+    expect(tweetThis).toHaveAttribute('id', 'tweet-quote')
+    expect(tweetThis).toHaveAttribute('target', '_blank')
+    expect(tweetThis).toHaveAttribute(
+      'href',
+      'https://twitter.com/intent/tweet?text=%22new_mock_quote%22+new_mock_author',
+    )
   })
 })
--- src/components/quote/quote.tsx
+++ src/components/quote/quote.tsx
@@ -1,20 +1,33 @@
 import '../../styles/global.css'
 import { FC } from 'react'
 
-type QuoteProps = {
-  author: string
-  text: string
+import { useQuote } from './quote.hooks'
+
+const tweetURL = (text: string, author: string) => {
+  const url = new URL('https://twitter.com/intent/tweet')
+  url.searchParams.set('text', `"${text}" ${author}`)
+
+  return url.toString()
 }
 
-export const Quote: FC<QuoteProps> = ({ author, text }) => {
+export const Quote: FC = () => {
+  const { data, refetch } = useQuote()
+
   return (
     <div id="quote-box">
       <figure>
-        <blockquote id="text">{text}</blockquote>
-        <figcaption id="author">{author}</figcaption>
+        <blockquote id="text">{data?.text}</blockquote>
+        <figcaption id="author">{data?.author}</figcaption>
       </figure>
-      <button id="new-quote">New Quote</button>
-      <a href="#!" id="tweet-quote">
+      <button id="new-quote" onClick={() => refetch()}>
+        New Quote
+      </button>
+      <a
+        href={tweetURL(data?.text || '', data?.author || '')}
+        id="tweet-quote"
+        target="_blank"
+        rel="noreferrer"
+      >
         Tweet this quote!
       </a>
     </div>
