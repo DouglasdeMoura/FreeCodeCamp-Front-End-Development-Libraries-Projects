32838af5d6405a6be798281f42b311e7a85646a9
Douglas Moura <douglas.ademoura@gmail.com>
2022-06-26 19:19:46 -0300

    feat(ErrorBoundary): create component

---
+++ src/components/error-boundary/error-boundary.tsx
@@ -0,0 +1,33 @@
+import { FC } from 'react'
+import { ErrorBoundary as ReactErrorBoundary } from 'react-error-boundary'
+
+type ErrorBoundaryProps = {
+  children: React.ReactNode
+  showTryAgain?: boolean
+  tryAgainText?: string
+}
+
+export const ErrorBoundary: FC<ErrorBoundaryProps> = ({
+  children,
+  showTryAgain = true,
+  tryAgainText,
+}) => (
+  <ReactErrorBoundary
+    fallbackRender={({ error, resetErrorBoundary }) => (
+      <div
+        role="alert"
+        className="py-5 px-6 mb-4 text-base text-red-700 bg-red-100 rounded-lg"
+      >
+        <h2>Something went wrong:</h2>
+        <pre className="whitespace-pre-line">{error.message}</pre>
+        {showTryAgain && (
+          <button onClick={() => resetErrorBoundary()}>
+            {tryAgainText || 'Try again'}
+          </button>
+        )}
+      </div>
+    )}
+  >
+    {children}
+  </ReactErrorBoundary>
+)
