42c62d3f9c1c02aa04449369f9d4095b9bb51ff6
Douglas Moura <douglas.ademoura@gmail.com>
2022-07-01 11:49:39 -0300

    feat(DrumMachine): create component

--- src/pages/drum-machine/drum-machine.spec.tsx
+++ src/pages/drum-machine/drum-machine.spec.tsx
@@ -1,11 +1,140 @@
-import { render } from '@testing-library/react'
+/* eslint-disable testing-library/no-node-access */
+/* eslint-disable testing-library/no-container */
+import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import { DrumMachine } from './drum-machine'
 
+const keys = 'QWEASDZXC'
+
 describe('<DrumMachine />', () => {
-  it('should render the DrumMachine component', () => {
+  test('I should be able to see an outer container with a corresponding id="drum-machine" that contains all other elements.', () => {
     render(<DrumMachine />)
 
-    expect(true).toBeTruthy()
+    const wrapper = screen.getByTestId('drum-machine')
+
+    expect(wrapper).toHaveAttribute('id', 'drum-machine')
+  })
+
+  test('Within #drum-machine I can see an element with a corresponding id="display".', () => {
+    render(<DrumMachine />)
+
+    const display = screen.getByTestId('display')
+    expect(display).toHaveAttribute('id', 'display')
+  })
+
+  test('Within #drum-machine I can see 9 clickable drum pad elements, each with a class name of drum-pad, a unique id that describes the audio clip the drum pad will be set up to trigger, and an inner text that corresponds to one of the following keys on the keyboard: Q, W, E, A, S, D, Z, X, C. The drum pads MUST be in this order.', async () => {
+    const { container } = render(<DrumMachine />)
+    const drumpads = container.querySelectorAll<HTMLButtonElement>('.drum-pad')
+
+    expect(drumpads).toHaveLength(9)
+
+    const audioClips: string[] = []
+    const text: string[] = []
+
+    drumpads.forEach((drumpad) => {
+      const audio = drumpad.querySelector<HTMLAudioElement>('audio')
+
+      if (audio?.src) {
+        audioClips.push(audio?.src)
+      }
+
+      if (drumpad.textContent) {
+        text.push(drumpad.textContent)
+      }
+    })
+
+    // check if every drumpad has an unique audio clip
+    expect([...new Set(audioClips)]).toEqual(audioClips)
+
+    // check the order of the buttons
+    expect(text.join('')).toBe(keys)
+  })
+
+  test('Within each .drum-pad, there should be an HTML5 audio element which has a src attribute pointing to an audio clip, a class name of clip, and an id corresponding to the inner text of its parent .drum-pad (e.g. id="Q", id="W", id="E" etc.).', () => {
+    const { container } = render(<DrumMachine />)
+
+    const drumpads = container.querySelectorAll('.drum-pad')
+
+    drumpads.forEach((drumpad) => {
+      const audio = drumpad.querySelector('audio')
+
+      expect(audio).toHaveAttribute('id', drumpad.textContent)
+      expect(audio).toHaveAttribute('class', 'hidden clip')
+      expect(audio).toHaveAttribute('src')
+      expect(audio?.src).toMatch(/\.mp3$/)
+    })
+  })
+
+  test('When I click on a .drum-pad element, the audio clip contained in its child audio element should be triggered.', async () => {
+    const play = jest.fn()
+
+    const { container } = render(<DrumMachine />)
+
+    const drumpads = container.querySelectorAll<HTMLButtonElement>('.drum-pad')
+
+    drumpads.forEach((drumpad) => {
+      const audio = drumpad.querySelector('audio') as HTMLAudioElement
+      audio.play = play
+      drumpad.click()
+    })
+
+    await waitFor(() => {
+      expect(play).toHaveBeenCalledTimes(keys.length)
+    })
+  })
+
+  test('When I press the trigger key associated with each .drum-pad, the audio clip contained in its child audio element should be triggered (e.g. pressing the Q key should trigger the drum pad which contains the string Q, pressing the W key should trigger the drum pad which contains the string W, etc.).', async () => {
+    const play = jest.fn()
+
+    const { container } = render(<DrumMachine />)
+
+    const drumpads = container.querySelectorAll<HTMLButtonElement>('.drum-pad')
+
+    drumpads.forEach((drumpad) => {
+      const audio = drumpad.querySelector('audio') as HTMLAudioElement
+      audio.play = play
+    })
+
+    keys.split('').forEach((key) => {
+      userEvent.keyboard(`{${key}}`)
+    })
+
+    await waitFor(() => {
+      expect(play).toHaveBeenCalledTimes(keys.length)
+    })
+  })
+
+  test('When a .drum-pad is triggered, a string describing the associated audio clip is displayed as the inner text of the #display element (each string must be unique).', () => {
+    const audios = [
+      'Heater 1',
+      'Heater 2',
+      'Heater 3',
+      'Heater 4',
+      'Clap',
+      'Open HH',
+      "Kick n' Hat",
+      'Kick',
+      'Closed HH',
+    ]
+
+    const { container } = render(<DrumMachine />)
+
+    const drumpads = container.querySelectorAll<HTMLButtonElement>('.drum-pad')
+
+    drumpads.forEach(async (drumpad, index) => {
+      drumpad.click()
+      await waitFor(() => {
+        expect(screen.getByTestId('display')).toHaveTextContent(audios[index])
+      })
+    })
+
+    keys.split('').forEach(async (key, index) => {
+      userEvent.keyboard(`{${key}}`)
+
+      await waitFor(() => {
+        expect(screen.getByTestId('display')).toHaveTextContent(audios[index])
+      })
+    })
   })
 })
--- src/pages/drum-machine/drum-machine.tsx
+++ src/pages/drum-machine/drum-machine.tsx
@@ -1,5 +1,126 @@
-import { FC } from 'react'
+/* eslint-disable tailwindcss/no-custom-classname */
+import { FC, useEffect, useRef } from 'react'
+
+import { Button } from '../../components/button'
 
 export const DrumMachine: FC = () => {
-  return null
+  const displayRef = useRef<HTMLDivElement>(null)
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    const key = e.key.toUpperCase()
+    const button = document.querySelector<HTMLButtonElement>(
+      `button[id="${key}"]`,
+    )
+
+    if (button) {
+      button.classList.toggle('bg-blue-800')
+      button.click()
+
+      setTimeout(() => {
+        button.classList.toggle('bg-blue-800')
+      }, 100)
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown)
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [])
+
+  const drumPadElements = [
+    {
+      name: 'Heater 1',
+      audioClip: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
+      key: 'Q',
+    },
+    {
+      name: 'Heater 2',
+      audioClip: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
+      key: 'W',
+    },
+    {
+      name: 'Heater 3',
+      audioClip: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
+      key: 'E',
+    },
+    {
+      name: 'Heater 4',
+      audioClip: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3',
+      key: 'A',
+    },
+    {
+      name: 'Clap',
+      audioClip: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',
+      key: 'S',
+    },
+    {
+      name: 'Open HH',
+      id: 'D',
+      audioClip: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
+      key: 'D',
+    },
+    {
+      name: "Kick n' Hat",
+      audioClip: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
+      key: 'Z',
+    },
+    {
+      name: 'Kick',
+      id: 'X',
+      audioClip: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
+      key: 'X',
+    },
+    {
+      name: 'Closed HH',
+      audioClip: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
+      key: 'C',
+    },
+  ]
+
+  return (
+    // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+    <div
+      id="drum-machine"
+      data-testid="drum-machine"
+      className="flex gap-4 justify-center items-center w-screen h-screen"
+    >
+      <div className="flex flex-col gap-4 justify-center items-center p-2 bg-white rounded scale-150">
+        <div className="grid grid-cols-3 gap-2">
+          {drumPadElements.map((drumPadElement) => (
+            <Button
+              id={drumPadElement.key}
+              key={drumPadElement.key}
+              className="drum-pad"
+              data-key={drumPadElement.key}
+              onClick={(e) => {
+                e.currentTarget.querySelector('audio')?.play()
+                if (displayRef.current) {
+                  displayRef.current.innerText = drumPadElement.name
+                }
+              }}
+            >
+              {drumPadElement.key}
+              {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
+              <audio
+                id={drumPadElement.key}
+                src={drumPadElement.audioClip}
+                data-name={drumPadElement.name}
+                className="hidden clip"
+              />
+            </Button>
+          ))}
+        </div>
+        <div
+          id="display"
+          data-testid="display"
+          ref={displayRef}
+          aria-label="Audio clip"
+          className="flex justify-center items-center w-48  font-bold"
+        ></div>
+      </div>
+    </div>
+  )
 }
