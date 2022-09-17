adf23681baa14129c4b6829f21b9d1ddd1c56570
Douglas Moura <douglas.ademoura@gmail.com>
2022-09-17 17:10:50 -0300

    chore: add Vite plugin to read project custom paths

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import EnvironmentPlugin from 'vite-plugin-environment'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,6 @@ export default defineConfig({
       jsxRuntime: 'classic',
     }),
     EnvironmentPlugin(['API_BASE_URL']),
+    tsconfigPaths(),
   ],
 })
